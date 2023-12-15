369b7c78db995c26b3cbae606dbfc94e84ff5267
James Osunkiyesi <jaydhales@gmail.com>
2023-12-15 18:09:53 +0100

    Done

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "JayChain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Jay Chain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JAY"
 )
 
 var ID ids.ID
